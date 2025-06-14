5119000d1395c74cc86dacf9579fcb7915000b4c
kacper481 <kacper.salek32@gmail.com>
2025-06-14 08:35:39 +0200

    add generate answer function and print metadata if verbose flag included

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,17 +14,29 @@ from google.genai import types
 client = genai.Client(api_key=api_key)
 
 model = "gemini-2.0-flash-001"
-prompt = "why Gemini has so difficult documentation?"
 
-def generate_text(prompt):
+if len(sys.argv) > 1:
+    user_prompt = sys.argv[1]
+else:
+    print("Usage: python main.py <model_name>")
+    sys.exit(1)
+
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
+def generate_answer(user_prompt):
     response = client.models.generate_content(
         model=model,
-        contents=prompt
+        contents=messages,
     )
     return response
 
-if __name__ == "__main__":
-    response = generate_text(prompt)
-    print(response.text)
+def print_metadata(response):
+    print("User prompt:", user_prompt)
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+if __name__ == "__main__":
+    response = generate_answer(user_prompt)
+    print(response.text)
+    if "--verbose" in sys.argv:
+        print_metadata(response)
